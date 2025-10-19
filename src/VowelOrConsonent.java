8026c6285c32775f033ebc2c0c0ff2fdce20caaa
HP <raisaantony1989@gmail.com>
2025-10-19 22:51:46 +0530

    Practice Java Oct 19

---
+++ src/VowelOrConsonent.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VowelOrConsonent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character");
+        String input = sc.next();
+        char c = input.charAt(0);
+        if(c == 'a' || c=='A' || c=='e'|| c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U') {
+            System.out.println("Character entered is vowel");
+        }
+            else{
+                System.out.println("Character entered is a consonent");
+            }
+        }
+    }
+
