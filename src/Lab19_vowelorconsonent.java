b9142fd449de11edceb96619228c96209d8cfff7
HP <raisaantony1989@gmail.com>
2025-10-19 22:53:48 +0530

    Practice Java Oct 19

--- src/Lab19_vowelorconsonent.java
+++ src/Lab19_vowelorconsonent.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VowelOrConsonent {
+public class Lab19_vowelorconsonent {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the character");
